f239e768bed619067f33fabc5dbea120c0960203
kuuttikeltto <ytkuuddi@gmail.com>
2022-11-13 23:05:00 +0200

    new excercise

---
+++ Osa 1/5. Ehtorakenne/keittoa.py
@@ -0,0 +1,11 @@
+name = input("Mikä on nimesi: ")
+
+portion = 5.90
+
+if name != "Jerry":
+    amount = float(input("Kuinka monta annosta keittoa: "))
+
+    print(f"Kokonaishinta on {portion * amount}")
+
+print("Seuraava!")
+
